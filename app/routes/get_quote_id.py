1b308ad4987ae3d6db4dddeadca747fcbd6489e8
fauzaanirsyaadi <56173199+fauzaanirsyaadi@users.noreply.github.com>
2021-04-21 14:10:23 +0700

    quote

--- app/routes/get_quote_id.py
+++ app/routes/get_quote_id.py
@@ -2,13 +2,11 @@ from app import app
 from flask import jsonify
 from ..models.quotes import quotes
 
-@app.route('/get_quote_id/')
-def get_quote_id():
+@app.route('/get_quote_id/<id>')
+def get_quote_id(id):
+    quote = quotes.query.filter_by(quotes_id=id).first_or_404()
+    return{
+        'quotes id': quote.quotes_id, 
+        'quotes value': quote.quotes_value, 
 
-    return jsonify([
-        {
-            'quotes id': user.quotes_id, 
-			'quotes value': user.quotes_value, 
-
-            } for user in quotes.query.all()
-    ])
+        }
